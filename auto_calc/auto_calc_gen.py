055a9a518167e00cc598a15f05755a8edfe602c0
yuantao327 <ytliang@fiberhome.com>
2016-11-18 10:41:01 +0800

    updated

--- auto_calc/auto_calc_gen.py
+++ auto_calc/auto_calc_gen.py
@@ -33,51 +33,24 @@ style.font = font
 style.alignment = alignment
 #style.borders = borders
 
-#a_rand_2 = random.choice((1,2,3,4))
-#a_rand_3 = random.choice((1,2,3))
-#a_rand_4 = random.choice((1,2))
-#a_rand_5 = 1
-
-
 # AAAxA=BBBB
-def calc_0(offset_col,offset_row ):
-    mult_b = random.choice((2,3,4))
-    if mult_b==2:
-        mult_a_1 = random.choice((1,2,3,4))
-        mult_a_2 = random.choice((1,2,3,4))
-        mult_a_3 = random.randint(0,9)
-    elif mult_b==3:
-        mult_a_1 = random.choice((1,2,3))
-        mult_a_2 = random.choice((1,2,3))
-        mult_a_3 = random.randint(0,9)
-    elif mult_b==4:
-        mult_a_1 = random.choice((1,2))
-        mult_a_2 = random.choice((1,2))
-        mult_a_3 = random.randint(0,9)
-    elif mult_b==5:
-        mult_a_1 = 1
-        mult_a_2 = 1
-        mult_a_3 = random.randint(0,9)
-    else:
-        mult_a_1 = 1
-        mult_a_2 = 1
-        mult_a_3 = 1
-
-    mult_a= int(str(mult_a_3)+str(mult_a_2)+str(mult_a_1))
+def calc_gen(offset_col,offset_row ):
+    mult_b = random.randint(2,9)
+    mult_a = random.randint(10,999)
     mult = mult_a*mult_b
     sheet.col(offset_col+0).width = 2000
     sheet.col(offset_col+1).width = 600
     sheet.col(offset_col+2).width = 2000
     sheet.col(offset_col+3).width = 600
     sheet.col(offset_col+4).width = 2000
-    sheet.write(1,offset_col+0,mult_a,style)
-    sheet.write(1,offset_col+1,'x',style)
-    sheet.write(1,offset_col+2,mult_b,style)
-    sheet.write(1,offset_col+3,'=',style)
-    sheet.write(1,offset_col+4,mult,style)
-    print mult_a
-    print mult_b
-    print mult
+    sheet.write(offset_row,offset_col+0,mult_a,style)
+    sheet.write(offset_row,offset_col+1,'x',style)
+    sheet.write(offset_row,offset_col+2,mult_b,style)
+    sheet.write(offset_row,offset_col+3,'=',style)
+    sheet.write(offset_row,offset_col+4,mult,style)
+#    print mult_a
+#    print mult_b
+#    print mult
 
     return;
 
@@ -86,8 +59,15 @@ for sheet_cnt in range(1,6):
     sheet_name = 'sheet'+str(sheet_cnt)
     sheet  = workbook.add_sheet(sheet_name,cell_overwrite_ok=True)
     sheet.write_merge(0, 0, 0, 10, 'Maths excersise '+sheet_name,style) # Merges row 0's columns 0 through 3.
-    calc_0(0,1)
-    calc_0(6,1)
+    for i in range(1,22):
+        calc_gen(0,i)
+        calc_gen(6,i)
+
+    for i in range(23,44):
+        calc_gen(0,i)
+        calc_gen(6,i)
+
+
 workbook.save('result.xls')
 print '创建excel文件完成！'
 
